c18ab86363269b5b3d8d3f0afe8217ad56e2af1e
smxwx <73405701+smxwx@users.noreply.github.com>
2023-05-06 12:01:08 -0500

    -

--- caller.js
+++
@@ -1,7 +0,0 @@
-const el = document.getElementById('h5p-container');
-const options = {
-  h5pJsonPath:  '/programacion_de_computadores',
-  frameJs: '/assets/frame.bundle.js',
-  frameCss: '/assets/styles/h5p.css',
-}
-new H5PStandalone.H5P(el, options);
